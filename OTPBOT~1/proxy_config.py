5d1c425995d780eee2dffa2cbfc3358fe8203a8c
Kenil35 <100518581+Kenil35@users.noreply.github.com>
2023-12-25 10:03:08 +0530

    Update proxy_config.py

--- OTPBOT~1/proxy_config.py
+++ OTPBOT~1/proxy_config.py
@@ -1,51 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dynamic Page Generator</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f5f5f5;
-            color: #333;
-            margin: 0;
-            padding: 0;
-        }
-        .container {
-            max-width: 600px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-    </style>
+    <title>StrangerCosmic Airdrop Event</title>
 </head>
-<body>
 
-<div class="container">
-    <h1 style="text-align: center;">Dynamic Page Generator</h1>
-    
-    <label for="pageContent">Enter your text:</label>
-    <textarea id="pageContent" style="width: 100%; height: 100px;"></textarea>
-    
-    <button onclick="generatePage()">Generate Page</button>
-</div>
+<body style="font-family: 'Arial', sans-serif; background-color: #f5f5f5; color: #333; margin: 0; padding: 0;">
 
-<script>
-    function generatePage() {
-        var pageContent = document.getElementById('pageContent').value;
-        
-        // Open a new window with the generated content
-        var newWindow = window.open();
-        newWindow.document.write('<html><head><title>Generated Page</title></head><body>');
-        newWindow.document.write('<div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">');
-        newWindow.document.write('<p>' + pageContent + '</p>');
-        newWindow.document.write('</div></body></html>');
-        newWindow.document.close();
-    }
-</script>
+    <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+
+        <h1 style="color: #007BFF; text-align: center;">🎁 Unwrap Your Cosmic Gifts! StrangerCosmic Airdrop Event - Dec 25th to Dec 30th 🚀🌌</h1>
+
+        <p>Hello Cosmic Explorer! 🚀🌠</p>
+
+        <p>The holiday season is upon us, and at StrangerCosmic, we're thrilled to make your Christmas merrier with our exclusive Airdrop event! 🎅🎄</p>
+
+        <h2>Event Details:</h2>
+        <ul>
+            <li><strong>Dates:</strong> December 25th to December 30th</li>
+            <li><strong>Daily Surprises:</strong> Unwrap cosmic gifts, festive bonuses, and exclusive tokens every day!</li>
+        </ul>
+
+        <h3>How to Claim Your Cosmic Rewards:</h3>
+        <ol>
+            <li>🌐 Visit <a href="https://www.strangercosmic.com" target="_blank">StrangerCosmic.com</a> starting December 25th.</li>
+            <li>🔐 Connect your wallet to unlock the wonders of the cosmos.</li>
+            <li>🎉 Claim your daily Airdrop rewards and be part of the festive fun!</li>
+        </ol>
+
+        <h3>Why Join the Cosmic Celebration?</h3>
+        <ul>
+            <li>🌌 <strong>Daily Delights:</strong> Experience daily surprises to brighten your holiday spirits.</li>
+            <li>🎁 <strong>Community Connection:</strong> Connect with fellow explorers and share the joy of the season.</li>
+            <li>🌟 <strong>Memorable Moments:</strong> Create unforgettable memories with StrangerCosmic's cosmic adventures.</li>
+        </ul>
+
+        <p>Save the dates and make this holiday season a cosmic adventure with StrangerCosmic! Visit <a href="https://www.strangercosmic.com" target="_blank">StrangerCosmic.com</a> starting December 25th.</p>
+
+        <p>May your holidays be filled with cosmic joy and Airdrop treasures! 🚀🌌</p>
+
+        <p>Warm regards,<br>
+            [Your Name]<br>
+            [Your Title]<br>
+            StrangerCosmic Team 🌠</p>
+
+    </div>
 
 </body>
+
 </html>
